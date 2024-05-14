17af82837e739f17336e9669f7d2aeb5c9adbbe3
seoyeon1123 <lsy_0906@naver.com>
2024-05-14 12:35:01 +0900

    feat : deleteBtn 추가 & 수정

--- lectures/project/todo/main.js
+++ lectures/project/todo/main.js
@@ -8,26 +8,53 @@
 const $addBtn = document.querySelector('#add-btn');
 const $inputBox = document.querySelector('#input-box');
 const $todoList = document.querySelector('#to-do-list');
+const $deleteBtn = document.querySelector('#delete-all-btn');
+const $listCount = document.querySelector('#todoList-count');
+
+$addBtn.addEventListener('click', function () {
+  if ($inputBox.value.trim() !== '') {
+    const $li = document.createElement('li');
+    const $deleteIcon = document.createElement('i');
+
+    $li.textContent = $inputBox.value.trim();
+    $deleteIcon.classList.add('delete-icon', 'fas', 'fa-trash-alt');
+
+    $li.appendChild($deleteIcon);
+    $todoList.appendChild($li);
+    $inputBox.value = '';
+
+    // 삭제 아이콘을 클릭하면 리스트가 삭제되도록 이벤트 추가
+    $deleteIcon.addEventListener('click', function () {
+      $todoList.removeChild($li);
+      updateListCount(); // 항목이 삭제될 때마다 목록의 개수 업데이트
+    });
+
+    updateListCount(); // 항목이 추가될 때마다 목록의 개수 업데이트
+  }
+});
+
+//전체 삭제 버튼 (리스트 개수 업데이트)
+$deleteBtn.addEventListener('click', function () {
+  while ($todoList.firstChild) {
+    $todoList.removeChild($todoList.firstChild);
+  }
+
+  updateListCount();
+});
+
+//리스트 업데이트
+function updateListCount() {
+  const itemCount = $todoList.querySelectorAll('li').length;
+  $listCount.textContent = itemCount + ' tasks'; // 목록의 개수를 "tasks" 뒤에 추가하여 업데이트합니다.
+  if (itemCount === 0) {
+    $listCount.textContent = 'tasks';
+  }
+}
 
 $inputBox.addEventListener('keyup', function (e) {
-  if (e.target.value) {
+  if (e.target.value.trim() !== '') {
     $addBtn.classList.add('active');
   } else {
     $addBtn.classList.remove('active');
   }
-});
-
-$addBtn.addEventListener('click', function () {
-  const $li = document.createElement('li');
-  const $span = document.createElement('span');
-  const $i = document.createElement('i');
-
-  $i.classList.add('fas', 'fa-trash');
-  $span.classList.add('icon');
-  $span.appendChild($i);
-
-  $li.textContent = $inputBox.value;
-  $li.appendChild($span); // 아이콘을 span 뒤에 추가합니다.
-  $todoList.appendChild($li);
-  $inputBox.value = '';
 });
